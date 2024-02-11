4da4880543411194a664619382b672dbba31ec7a
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2024-02-10 23:55:20 -0500

    Initial Commit

---
+++ Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Paul Leiva on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
