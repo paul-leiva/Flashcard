adbee00a1f2872cb7e16aa747d27eeff265e3149
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2024-02-11 00:43:51 -0500

    Step 1: Get oriented with the SwiftUI project layout

--- Flashcard/ContentView.swift
+++ Flashcard/ContentView.swift
@@ -5,20 +5,29 @@
 //  Created by Paul Leiva on 2/10/24.
 //
 
+// Import the SwiftUI Framework in any file you want to use SwiftUI
 import SwiftUI
 
+// Define the ContentView structure, which conforms to the View protocol
 struct ContentView: View {
+    // Define the body property, required by View protocol
+    // Return any object that conforms to the View protocol
     var body: some View {
+        // Vertical stack (VStack) to arrange views vertically
         VStack {
+            // Image view with the system's globe icon
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .imageScale(.large) // Set the image scale to large
+                .foregroundColor(.accentColor) // set the image color to the tint color
+            
+            // Text view displaying the "Hello, world!" string
             Text("Hello, world!")
         }
-        .padding()
+        .padding() // Apply padding to the entire VStack
     }
 }
 
+// Preview the ContentView in the canvas
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Flashcard/FlashcardApp.swift
+++ Flashcard/FlashcardApp.swift
@@ -7,11 +7,17 @@
 
 import SwiftUI
 
+// The @main attribute indicates that FlashcardApp is the entry point for the app.
 @main
 struct FlashcardApp: App {
+    // The `body` property is required for any SwiftUI App. It defines the content of the app.
     var body: some Scene {
+        // A Scene represents a single user interface within the app.
         WindowGroup {
+            // The WindowGroup represents the main window of the app
             ContentView()
+            // ContentView is the main view or the root view of the app.
+            // This is where you start defining the structure and content of your app's user interface.
         }
     }
 }
